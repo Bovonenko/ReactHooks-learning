794cac6e1c6e75ab72aa1f79f0c383486154b24c
Rostyslav <bovonenko@icloud.com>
2022-07-19 10:39:17 +0200

    useMemo

---
+++ src/UseMemo.js
@@ -0,0 +1,31 @@
+import React, {useState, useMemo} from "react";
+const UseMemo = () => {
+    const [number, setNumber] = useState(0);
+    const [dark, setDark] = useState(false);
+
+    const doubleNumber = useMemo(() => slowFunction(number), [number]);
+    const themeStyles = {
+        backgroundColor: dark ? 'black' : 'white',
+        color: dark ? 'white' : 'black'
+    }
+
+
+
+    return (
+        <>
+            <input 
+                value={number}
+                type="number"
+                onChange={e => setNumber(parseInt(e.target.value))} />
+            <button onClick={() => setDark(prevDark => !prevDark)}>Change Theme</button>
+            <div style={themeStyles}>{doubleNumber}</div>
+        </>
+    )
+}
+
+function slowFunction(num) {
+    for (let i = 0; i < 1000000000; i++) {}
+    return num * 2;
+}
+
+export default UseMemo;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import UseMemo from './UseMemo';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-    <App/>,
+    // <App/>,
+    <UseMemo/>,
   document.getElementById('root')
 );
 
